a9f9a65b5ffed33acbfab0fff3ad17d32b2784a3
muyed moktadir <muyed004@gmail.com>
2022-04-18 21:52:51 +0600

    add socialsignwithgoogle

---
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+
+const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorElement;
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (error) {
+    errorElement = (
+      <p className="text-danger">
+        Error: {error?.message} {error?.message}
+      </p>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
+  return (
+    <div>
+      {errorElement}
+      <button onClick={() => signInWithGoogle()}>Google SignIn</button>
+    </div>
+  );
+};
+
+export default SocialLogin;
