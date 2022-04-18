8eff889a7762a76d2246779e99a5b0b9182769ec
muyed moktadir <muyed004@gmail.com>
2022-04-18 23:14:49 +0600

    readme file add

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from "./pages/Shared/Footer/Footer";
 import RequireAuth from "./pages/Login/RequireAuth/RequireAuth";
 import CheckOut from "./pages/Checkout/CheckOut";
 import Blog from "./pages/Blog/Blog";
+import About from "./pages/About/About";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           </RequireAuth>
         }></Route>
         <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import OthersPackage from '../OthersPackage/OthersPackage';
 import OthersPackages from '../OthersPackages/OthersPackages';
 import Services from '../Services/Services';
 
